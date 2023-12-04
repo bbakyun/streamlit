ea1e8682f07c63660483432d4b0ff2de084c8847
bbakyun <63243102+bbakyun@users.noreply.github.com>
2023-12-04 18:34:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
+# streamlit.py
+
 import streamlit as st
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 # 데이터를 저장할 변수
 stored_data = {}
 
-# 업데이트 데이터를 처리하는 함수
-def update_data(data):
+# 업데이트 데이터를 처리하는 엔드포인트
+@app.route('/update_data', methods=['POST'])
+def update_data():
     try:
+        data = request.get_json()
         global stored_data
         # 기존 데이터에 추가
         stored_data.update(data)
-        return {'message': 'Data received and updated successfully.'}
+        return jsonify({'message': 'Data received and updated successfully.'})
     except Exception as e:
-        return {'error': str(e)}
+        return jsonify({'error': str(e)})
 
 # Streamlit 앱
 def main():
@@ -20,11 +27,6 @@ def main():
     # 저장된 데이터를 출력
     st.write('Stored Data:', stored_data)
 
-# Streamlit 앱 실행
 if __name__ == '__main__':
-    # 업데이트 데이터를 처리하는 함수를 호출하여 데이터를 업데이트
-    result = update_data({"example_key": "example_value"})
-    st.write(result)  # 업데이트 결과 출력
-
     # Streamlit 앱 실행
     main()
