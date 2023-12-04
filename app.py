fb0f0b93428c73a5dd5ec5e5eb857cb9fe207c7c
bbakyun <63243102+bbakyun@users.noreply.github.com>
2023-12-04 18:28:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 import streamlit as st
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
 # 데이터를 저장할 변수
 stored_data = {}
 
-# 업데이트 데이터를 처리하는 엔드포인트
-@app.route('/update_data', methods=['POST'])
-def update_data():
+# 업데이트 데이터를 처리하는 함수
+def update_data(data):
     try:
-        data = request.get_json()
         global stored_data
         # 기존 데이터에 추가
         stored_data.update(data)
-        return jsonify({'message': 'Data received and updated successfully.'})
+        return {'message': 'Data received and updated successfully.'}
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return {'error': str(e)}
 
 # Streamlit 앱
 def main():
@@ -26,5 +20,11 @@ def main():
     # 저장된 데이터를 출력
     st.write('Stored Data:', stored_data)
 
+# Streamlit 앱 실행
 if __name__ == '__main__':
+    # 업데이트 데이터를 처리하는 함수를 호출하여 데이터를 업데이트
+    result = update_data({"example_key": "example_value"})
+    st.write(result)  # 업데이트 결과 출력
+
+    # Streamlit 앱 실행
     main()
